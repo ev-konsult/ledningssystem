d9b173b78ff58bb180b64f6ac0766838c6149edd
ad222kr <ad222kr@student.lnu.se>
2016-02-01 13:23:18 +0100

    Test fixtures and seeds for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 User.create(name: "Admin", password: "adminpassword", password_confirmation: "adminpassword", admin: true)
 User.create(name: "First User", password: "firstpassword", password_confirmation: "firstpassword")
 User.create(name: "Second User", password: "secondpassword", password_confirmation: "secondpassword")
+
+
+User.first.articles << Article.create(title: "Hejsan hoppsan", body: "Jag gillar katter dom är mycket söta. Hejdå")
+User.find(2).articles << Article.create(title: "Lorem Ipsum", body: "Hej hej hejsan hejsan hej hej hejsan hejsan")
