fa481b1d132fabfa0a00bd06f4eeb85a38f0f666
Kristoffer Karlsson <kk222hk@student.lnu.se>
2016-02-10 18:11:55 +0100

    fix: seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create(name: "Admin", password: "adminpassword", password_confirmation: "ad
   @user = User.create(name: name, email: email, phone_number: phone_number, ssn: ssn, password: password)
 end
 
-# Skapar artiklar
+# Skapar artiklar för första och sista användaren
 User.first.articles << Article.create(title: "Hejsan hoppsan", body: "Jag gillar katter dom är mycket söta. Hejdå")
 User.last.articles << Article.create(title: "Lorem Ipsum", body: "Hej hej hejsan hejsan hej hej hejsan hejsan")
 
