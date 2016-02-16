c7298444e4e2bf4a9bdc7b66c4de77138c0e3e5b
ad222kr <ad222kr@student.lnu.se>
2016-02-16 15:14:27 +0100

    feat w/ @kk222hk: Adding users to a task when creating the task implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,14 +5,29 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    @user_ids = params[:users][:user_id]
 
     if @task.save
+      # Add the users to the task if the task can be saved.
+      # Else it somehow trys to validate a user and cannot save the task...
+      # TODO: Try to figure out why it behaves this way
+      @user_ids.each do |id|
+        next if id.blank?
+        @task.users << User.find(id)
+      end
       flash[:success] = "Blabalbal"
       redirect_to @task
     else
       flash.now[:danger] = "Bla bla!"
       render 'new'
     end
+  end
+
+  def update
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   private
