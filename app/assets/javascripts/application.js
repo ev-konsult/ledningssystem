c0b41b7686d083646ee6010a768371e4c9504087
Holmewi <jh222qr@student.lnu.se>
2016-02-16 13:33:06 +0100

    fix: Issue with the menu not showing when window is resized

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,4 +42,9 @@ $('#username').focus();
 
 });
 
-
+$(window).on('resize', function () {
+  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+});
+$(window).on('resize', function () {
+  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+});
