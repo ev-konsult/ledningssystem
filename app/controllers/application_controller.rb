5cb4e6a00b3cfe08cd86394dd49c4338aa8d0065
rf222cz <rf222cz@student.lnu.se>
2016-02-23 11:29:12 +0100

    fix w/ @kk222kn @mj223dg @ad222kr cache-buster for no return-button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_cache_buster
   include SessionsHelper
 
   protected
-
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+  
   def authorize_login
     redirect_to login_path unless logged_in?
   end
