80065441ed820789baedba2c1cf0aa13e2f752e3
Kristoffer Karlsson <kk222hk@student.lnu.se>
2016-02-24 00:26:49 +0100

    test: more controller tests for tasks

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -5,6 +5,9 @@ class TasksControllerTest < ActionController::TestCase
   def setup
     # Sätter current user till admin
     session[:user_id] = User.where(admin: true).take.id
+
+    @task = tasks(:one)
+    @user = users(:one)
   end
 
   test "index" do
@@ -29,5 +32,38 @@ class TasksControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to task_path(assigns(:task))
+  end
+
+  test "should edit task" do
+    # Same title
+    title = "Städa köket"
+
+    # New description
+    description = "Nu måste praktikanterna skrubba taket också"
+
+    patch :update, id: @task, task: { title: title, description: description,
+                                      start: DateTime.new(2017, 4, 5),
+                                      end: DateTime.new(2017, 5, 6) },
+                              user_ids: [users(:one).id, users(:two)]
+    assert_equal "Uppgiften uppdaterades!", flash[:success]
+    assert_redirected_to @task
+
+    @task.reload
+    assert_equal title, @task.title
+    assert_equal description, @task.description
+  end
+
+  # Tests that you can't add a user to a task via an update, when the
+  # user is already assigned to the task
+  test "should not add duplicate users to task" do
+    @task.users << @user
+
+    assert_no_difference('@task.users.count') do
+      patch :update, id: @task, task: { title: "Lorem ipsum lorem ipsum",
+                                        description: "Lorem ipsum lorem ipsum lorem ipsum",
+                                        start: DateTime.new(2017, 4, 5),
+                                        end: DateTime.new(2017, 5, 6) },
+                                user_ids: [@user.id]
+    end
   end
 end
