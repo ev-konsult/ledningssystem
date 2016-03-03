1857051f9de6953d2d40363277f2d62daf4c62a2
Kristoffer Karlsson <kk222hk@student.lnu.se>
2016-03-03 22:02:04 +0100

    doc: forgot to mention that my latest commits where w/ @ad222kr , @rebeccafransson , @mj223dg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cache_buster
   include SessionsHelper
+  #
 
   protected
   def set_cache_buster
