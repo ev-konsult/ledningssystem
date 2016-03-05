4e1caf7a557c47d40e2a8b1af036dadc8b621e70
Alex <ad222kr@student.lnu.se>
2016-03-05 01:50:18 +0100

    design: Pie-chart for tasks by status. Not translated yet

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,6 +1,26 @@
 class StatisticsController < ApplicationController
   def index
+    @tasks = {
+      not_started: 0,
+      in_progress: 0,
+      done: 0,
+      cancelled: 0
+    }
+
+    Task.all.each do |task|
+      case task.status.to_sym
+      when :not_started
+        @tasks[:not_started] += 1
+      when :done
+        @tasks[:done] += 1
+      when :in_progress
+        @tasks[:in_progress] += 1
+      when :cancelled
+        @tasks[:cancelled] += 1
+      end
+    end
   end
+
 
   def show
   end
