b72af7c1672f01991e08db5e8730a796be515975
Kristoffer Karlsson <kk222hk@student.lnu.se>
2016-03-08 20:58:59 +0100

    docs..?: database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,41 @@
 # Skapar admin med kontaktperson
-@admin_contact = ContactPerson.create(full_name: "Admins Contact Guy", phone_number: "0722222222", email: "admin@gmail.com")
+@admin_contact = ContactPerson.create(full_name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number, email: Faker::Internet.email)
 @hr = Role.create(description: "Äger rättigheter till personal", role_name_id: :human_resources, can_edit_staff: true, can_show_person_details_verbose: true)
 @manager = Role.create(description: "Äger rättigheter till att hantera uppgifter.", role_name_id: :project_manager, can_edit_tasks: true)
 @editorz = Role.create(description: "Äger rättigheter till nyheter.", role_name_id: :editor, can_edit_news: true)
 @admin_role = Role.create(description: "Äger rättigheter till hela systemet", role_name_id: :admin, can_edit_news: true, can_edit_tasks: true, can_edit_staff: true, can_edit_documents: true, can_show_person_details_verbose: true)
 
-User.create(user_name: "Admin", first_name: "Admin", last_name: "Adminsson",
+@admin = User.create(user_name: "Admin", first_name: "Lasse", last_name: "Karlsson",
             password: "adminpassword", password_confirmation: "adminpassword",
-            email: "admin@admin.com", ssn: "123456-1234", phone_number: "0701234123", contact_person: @admin_contact, role: @admin_role)
+            email: "admin@gmail.com", ssn: "910506-1234", phone_number: "0701234123", contact_person: @admin_contact, role: @admin_role,
+            contact_person: ContactPerson.create(full_name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number,
+                                                   email: "contact@gmail.com"))
 
-@editor = User.create(user_name: "TestUser", first_name: "Test", last_name: "User",
-            password: "testuserpassword", password_confirmation: "testuserpassword",
-            email: "test@user.com", ssn: "123456-1337", phone_number: "0701234999", contact_person: @admin_contact, role: @editorz)
+@admin.educations << Education.create(graduation: DateTime.now, school: Faker::University.name, name: Faker::Commerce.department)
 
-@hr_guy = User.create(user_name: "HumanResources", first_name: "Toby", last_name: "Flenderson",
-            password: "testuserpassword", password_confirmation: "testuserpassword",
-             email: "hrtest@user.com", ssn: "113456-1337", phone_number: "0701234919", contact_person: @admin_contact, role: @hr)
+@editor = User.create(user_name: "Bosse", first_name: "Bo", last_name: "Larsson",
+            password: "password", password_confirmation: "password",
+            email: "editor@gmail.com", ssn: "910506-1337", phone_number: "0701234999", contact_person: @admin_contact, role: @editorz,
+            contact_person: ContactPerson.create(full_name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number,
+                                                   email: "contact@gmail.com"))
 
-@project_leader = User.create(user_name: "ProjectLeader", first_name: "Michael", last_name: "Scott",
-            password: "testuserpassword", password_confirmation: "testuserpassword",
-             email: "mc@user.com", ssn: "123426-1337", phone_number: "0701334999", contact_person: @admin_contact, role: @manager)
+@editor.educations << Education.create(graduation: DateTime.now, school: Faker::University.name, name: Faker::Commerce.department)
+
+@hr_guy = User.create(user_name: "Toby", first_name: "Toby", last_name: "Flenderson",
+            password: "password", password_confirmation: "password",
+             email: "humanresources@gmail.com", ssn: "910506-1227", phone_number: "0701234919", contact_person: @admin_contact, role: @hr,
+             contact_person: ContactPerson.create(full_name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number,
+                                                    email: "contact@gmail.com"))
+
+@hr_guy.educations << Education.create(graduation: DateTime.now, school: Faker::University.name, name: Faker::Commerce.department)
+
+@project_leader = User.create(user_name: "Michael", first_name: "Michael", last_name: "Scott",
+            password: "password", password_confirmation: "password",
+             email: "michael@gmail.com", ssn: "910506-1357", phone_number: "0701334999", contact_person: @admin_contact, role: @manager,
+             contact_person: ContactPerson.create(full_name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number,
+                                                    email: "contact@gmail.com"))
+
+@project_leader.educations << Education.create(graduation: DateTime.now, school: Faker::University.name, name: Faker::Commerce.department)
 
 @role = Role.create(description: "Äger inga rättigheter", role_name_id: :user)
 
@@ -27,22 +43,30 @@ User.create(user_name: "Admin", first_name: "Admin", last_name: "Adminsson",
 99.times do |n|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
-  email = Faker::Internet.email
   phone_number = Faker::PhoneNumber.phone_number
   ssn = "920616-925#{n}"
   password = "password"
 
-  @contact_person = ContactPerson.create(full_name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number,
-                                         email: Faker::Internet.email)
+  contact_first_name = Faker::Name.first_name
+  contact_last_name = Faker::Name.last_name
+  @contact_person = ContactPerson.create(full_name: "#{contact_first_name}, #{contact_last_name}", phone_number: Faker::PhoneNumber.phone_number,
+                                         email: "#{contact_first_name}@gmail.com")
 
-  User.create(user_name: Faker::Name.name, first_name: first_name, last_name: last_name,
-              email: email, phone_number: phone_number, ssn: ssn, password: password, contact_person: @contact_person, role: @role)
+  @user = User.create(user_name: first_name, first_name: first_name, last_name: last_name,
+              email: "#{first_name}@gmail.com", phone_number: phone_number, ssn: ssn, password: password, contact_person: @contact_person, role: @role)
+
+  @user.educations << Education.create(graduation: DateTime.now, school: Faker::University.name, name: Faker::Commerce.department)
 end
 
-# Skapar artiklar för första och sista användaren
-User.first.articles << Article.create(title: "Hejsan hoppsan", body: "Jag gillar katter dom är mycket söta. Hejdå")
-User.last.articles << Article.create(title: "Lorem Ipsum", body: "Hej hej hejsan hejsan hej hej hejsan hejsan")
+20.times do |n|
+  # Random användare skapar artiklar
+  User.offset(rand(User.count)).first.articles << Article.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+end
 
-Task.create(title: "Städa köket", description: "Efter kontorsfesten är köket väldigt smutsigt. Praktikanterna måste städa bort alla ölburkar och skrubba väggarna.", start: DateTime.new(2017, 2, 3), end: DateTime.new(2017, 3, 4))
-
-Task.create(title: "Fixa bokföringen", description: "Det är för mycket siffror i bokföringen. Ersätt några med emojis.", start: DateTime.new(2017, 4, 5), end: DateTime.new(2017, 5, 6))
+20.times do |n|
+  @task = Task.create(title: Faker::Company.catch_phrase, description: "#{Faker::Company.catch_phrase}. #{Faker::Company.catch_phrase}. #{Faker::Company.catch_phrase}", start: DateTime.new(2018, 2, 3), end: DateTime.new(2018, 3, 4))
+  # Sätt random användare till uppgiften
+  4.times do |n|
+    @task.users << User.offset(rand(User.count)).first
+  end
+end
