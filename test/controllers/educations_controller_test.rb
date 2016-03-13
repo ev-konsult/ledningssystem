90568b44b47fe8481edd14564faf89e14552e0f2
Kristoffer Karlsson <kk222hk@student.lnu.se>
2016-03-13 16:14:32 +0100

    test: education controller tests

--- test/controllers/educations_controller_test.rb
+++ test/controllers/educations_controller_test.rb
@@ -1,5 +1,30 @@
 require 'test_helper'
 
 class EducationsControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:two)
+    @valid_education = { name: "Webbprogrammerare",
+                         school: "Linnéuniversitetet",
+                         graduation: DateTime.new(2017, 4, 5) }
 
+    @invalid_education = { name: "",
+                           school: "",
+                           graduation: "" }
+  end
+
+  test "valid education is created on user" do
+    assert @user.educations.empty?
+
+    assert_difference("@user.educations.count") do
+      @user.educations.create(@valid_education)
+    end
+  end
+
+  test "invalid educations is not created on user" do
+    assert @user.educations.empty?
+
+    assert_no_difference("@user.educations.count") do
+      @user.educations.create(@invalid_education)
+    end
+  end
 end
