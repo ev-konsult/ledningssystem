6d795b81180e3da45c8b19d46cdfaa01a7917a9a
Kristoffer Karlsson <kk222hk@student.lnu.se>
2016-03-16 17:24:26 +0100

    test: statistics index test #329

--- test/controllers/statistics_controller_test.rb
+++ test/controllers/statistics_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StatisticsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test fixture - simulates logging in as admin
+  def setup
+    session[:user_id] = users(:one).id
+  end
+
+  test "show statistics" do
+    # Makes sure we get an ok response
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tasks)
+
+    # Checks HTML for the charts
+    assert_select "div#chart-1"
+    assert_select "div#chart-2"
+    assert_select "div#chart-3"
+    assert_select "div#chart-4"
+  end
 end
